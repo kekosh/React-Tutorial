240e27e600be0d188f31d5ecc064c18f63fdfd45
kekosh <kenta0540@gmail.com>
2019-09-11 18:10:28 +0900

    update test.js

--- relern_js/test.js
+++ relern_js/test.js
@@ -2,7 +2,28 @@
 //     console.log("fizz"+i+1);
 // }
 
-array = ["hoge","fuga","nyan"];
-for(let x of array){
-    console.log(x);
+// array = ["hoge","fuga","nyan"];
+// for(let x of array){
+//     console.log(x);
+// }
+
+// function add(){
+//     let sum = 0;
+//     for(let i = 0, j = arguments.length; i < j; i++){
+//         sum += arguments[i];
+//     }
+//     return sum;
+// }
+
+// var r = add(2,3,4,5);
+
+function avg(...args){
+    let sum = 0;
+    for(let val of args){
+        sum += val;
+    }
+    return sum / args.length;
 }
+var r = avg(2,3,4,5);
+
+console.log(r);
