04a41095dfea5b441c5917ff4ce68182a0db3e05
kekosh <kenta0540@gmail.com>
2019-10-13 10:33:57 +0900

    Tutorial "Intaractive Component"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
 
 class Square extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {value: null};
+    }
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
         </button>
       );
     }
